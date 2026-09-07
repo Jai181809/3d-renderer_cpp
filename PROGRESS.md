63a05f37622c095d49ce25bdd267041a60fb0a4e
Jai181809 <mritunjayagraw007@gmail.com>
2026-09-08 02:46:24 +0530

    Basic io of pmp (not cleared)

--- PROGRESS.md
+++ PROGRESS.md
@@ -1,4 +1,12 @@
 [] getting ppm outputs
 [] vec3 class
 [] introduction to ray class
-[]
+[]  adding spheres
+[] Surfaces
+[] Antialising:- fixing jagged images
+[] Diffusing materials
+[] Metal
+[] Dielectrics(snells law)
+[] positioning cameras
+[] defocusing blur
+[] further talk on adv topics
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, Ray Tracer!" << std::endl;
+    // Image dimension
+    int Im_length = 256;
+    int Im_width = 256;
+    cout << "P3\n" << Im_length << ' ' << Im_width << "\n255\n";
+
+    for (int j = 0; j < Im_length; j++) {
+        for (int i = 0; i < Im_width; i++) {
+            auto r = double(i) / (Im_width-1);
+            auto g = double(j) / (Im_length-1);
+            auto b = 0.0;
+
+            int ir = int(255.999 * r);  // we multipply with 255.999 so that we dont missout/ overflow to 256
+            int ig = int(255.999 * g);
+            int ib = int(255.999 * b);
+
+            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
+
     return 0;
 }
