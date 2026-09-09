720e0cd8664b3e62bb2c78100a9dcf3a8ae48bab
Jai181809 <mritunjayagraw007@gmail.com>
2026-09-09 23:39:14 +0530

    header file made for vec3

--- src/vec3.cpp
+++ src/vec3.cpp
@@ -1,38 +1,59 @@
 #include "vec3.hpp"
 #include <cmath>
-#include <iostream>
+using namespace std;
 
-class vec3{
-    public :
-        double e[3];
-        vec3() : e{0,0,0} {}   // initializing default constructor
-        vec3(double x, double y, double z) : e{x,y,z} {}  // initializing constructor
+// Constructors
+vec3::vec3() : e{0, 0, 0} {}   // initializing default constructor
+vec3::vec3(double x, double y, double z) : e{x, y, z} {}   // initializing constructor
 
-        double x() const {return e[0];}  // functions to call elements of vectors (not changeable)
-        double y() const {return e[1];}
-        double z() const {return e[2];}
+// Getters
+double vec3::x() const { return e[0]; }   // functions to call elements of vectors (not changeable)
+double vec3::y() const { return e[1]; }
+double vec3::z() const { return e[2]; }
 
+// Member functions
+double vec3::mod() const {
+    return sqrt(mod_squared());
+}
 
-};
-vec3 operator+(const vec3& u, const vec3& v){
-    return vec3(u.x()+v.x(),u.y()+v.y(),u.z()+v.z());
+double vec3::mod_squared() const {
+    return x() * x() + y() * y() + z() * z();
 }
-vec3 operator-(const vec3& u, const vec3& v){
-    return vec3(u.x()-v.x(),u.y()-v.y(),u.z()-v.z());
+
+vec3 vec3::operator-() const {
+    return vec3(-x(), -y(), -z());
 }
-vec3 operator*(double t,const vec3& u){
-    return vec3(u.x()*t,u.y()*t,u.z()*t);
+
+// Non-member operators
+vec3 operator+(const vec3& u, const vec3& v) {   // here we called const as well as passsed vec3 by reference "&", which says that it wont copy(thus saving us space as we wont be making two another vectors "here u and v" as well time "copying takes time")
+    return vec3(u.x() + v.x(), u.y() + v.y(), u.z() + v.z());  //and const helps us says that we wont mutate the orignal vector
 }
-vec3 operator/(const vec3& u, double t){
-    return vec3(u.x()/t,u.y()/t,u.z()/t);
+
+vec3 operator-(const vec3& u, const vec3& v) {
+    return vec3(u.x() - v.x(), u.y() - v.y(), u.z() - v.z());
 }
-double dot(const vec3& v, const vec3& u){
-    return (v.x()*u.x()) + (v.y()*u.y()) + (v.z()*u.z());
+
+vec3 operator*(double t, const vec3& u) {
+    return vec3(u.x() * t, u.y() * t, u.z() * t);
 }
-vec3 cross(const vec3& v,const vec3& u){
-   return vec3((v.y()*u.z())-(v.z()*u.y()),(v.z()*u.x())-(v.x()*u.z()),(v.x()*u.y())-(v.y()*u.x()));
+
+vec3 operator/(const vec3& u, double t) {
+    return vec3(u.x() / t, u.y() / t, u.z() / t);
 }
-vec3 operator+=(vec3 &v,const vec3 &u){
+
+double dot(const vec3& v, const vec3& u) {
+    return (v.x() * u.x()) + (v.y() * u.y()) + (v.z() * u.z());
+}
+
+vec3 cross(const vec3& v, const vec3& u) {
+    return vec3(
+        (v.y() * u.z()) - (v.z() * u.y()),
+        (v.z() * u.x()) - (v.x() * u.z()),
+        (v.x() * u.y()) - (v.y() * u.x())
+    );
+}
+
+vec3& operator+=(vec3& v, const vec3& u) {
     v = v + u;
     return v;
 }
--- src/vec3.hpp
+++ src/vec3.hpp
@@ -1,28 +1,31 @@
 #pragma once
+
 #include <iostream>
-#include <cmath>
 
-using namespace std;
+class vec3 {
+public:
+    double e[3];
 
-class vec3{
-    public :
-        double e[3];
-        vec3() : e{0,0,0} {}   // initializing default constructor
-        vec3(double x, double y, double z) : e{x,y,z} {}  // initializing constructor
+    vec3();
+    vec3(double x, double y, double z);
 
-        double x() const ;  // functions to call elements of vectors (not changeable)
-        double y() const ;
-        double z() const;
+    double x() const;
+    double y() const;
+    double z() const;
 
-        double mod() const;
-        double mod_squared() const;
-        vec3 operator-() const;
+    double mod() const;
+    double mod_squared() const;
+
+    vec3 operator-() const;
 };
 
+// Non-member operator declarations
 vec3 operator+(const vec3& u, const vec3& v);
 vec3 operator-(const vec3& u, const vec3& v);
-vec3 operator*(double t,const vec3& u);
+vec3 operator*(double t, const vec3& u);
 vec3 operator/(const vec3& u, double t);
+
 double dot(const vec3& v, const vec3& u);
-vec3 cross(const vec3& v,const vec3& u);
-vec3 operator+=(vec3 &v,const vec3 &u);
+vec3 cross(const vec3& v, const vec3& u);
+
+vec3& operator+=(vec3& v, const vec3& u);
