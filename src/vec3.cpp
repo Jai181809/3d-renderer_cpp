48e2ef5d1af13e97626941231e9ffbdc8a553b6f
Jai181809 <mritunjayagraw007@gmail.com>
2026-09-09 23:34:40 +0530

    Tryna make vec3 into header file

--- src/vec3.cpp
+++ src/vec3.cpp
@@ -11,11 +11,14 @@ class vec3{
         double y() const {return e[1];}
         double z() const {return e[2];}
 
-        double dot(const vec3& v, const vec3& u){
-            return (v.x()*u.x()) + (v.y()*u.y()) + (v.z()*u.z());
+        double mod(){
+            double t = x()*x() + y()*y() + z()*z();
+            t = sqrt(t);
+            return t;
         }
-
-
+        vec3 operator~(){
+            return vec3(-x(),-y(),-z());
+        }
 };
 vec3 operator+(const vec3& u, const vec3& v){   // here we called const as well as passsed vec3 by reference "&", which says that it wont copy(thus saving us space as we wont be making two another vectors "here u and v" as well time "copying takes time")
     return vec3(u.x()+v.x(),u.y()+v.y(),u.z()+v.z());  //and const helps us says that we wont mutate the orignal vector
@@ -28,4 +31,13 @@ vec3 operator*(double t,const vec3& u){
 }
 vec3 operator/(const vec3& u, double t){
     return vec3(u.x()/t,u.y()/t,u.z()/t);
+}
+double dot(const vec3& v, const vec3& u){
+    return (v.x()*u.x()) + (v.y()*u.y()) + (v.z()*u.z());
+}
+vec3 scal(const vec3& v,const vec3& u){
+   return vec3((v.y()*u.z())-(v.z()*u.y()),(v.z()*u.x())-(v.x()*u.z()),(v.x()*u.y())-(v.y()*u.x()));
+}
+vec3 operator+=(vec3 &v,const vec3 &u){
+    return v+u;
 }
